340a7c166dbdcf68c4c8993b02493163c9fa046b
helloyxw <helloyxw@163.com>
2025-02-12 02:57:32 +0800

    增加vercel部署的配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  app.enableCors();
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp)
 }
-bootstrap();
+// 导出handler函数供Vercel使用
+let handler: any;
+// 初始化handler
+bootstrap().then(h => {
+  handler = h;
+});
+export { handler };
