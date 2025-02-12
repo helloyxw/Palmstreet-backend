7d42fe971cd6c04f8ed44d5376eaade68f1a3114
helloyxw <helloyxw@163.com>
2025-02-12 14:38:44 +0800

    部署vercel

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as serverless from 'serverless-http';
-import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as serverless from 'serverless-http';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, new ExpressAdapter());
+//   app.enableCors();
+//   await app.init();
+//   const expressApp = app.getHttpAdapter().getInstance();
+//   // 修复serverless调用问题，使用default属性
+//   return serverless.default(expressApp);
+// }
+// // 导出handler函数供Vercel使用
+// let handler: any;
+// // 初始化handler
+// bootstrap().then(h => {
+//   handler = h;
+// });
+// export { handler };
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as serverless from 'serverless-http';
+// import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  // 修复serverless调用问题，使用default属性
-  return serverless.default(expressApp);
+  await app.listen(3000)
 }
-// 导出handler函数供Vercel使用
-let handler: any;
-// 初始化handler
-bootstrap().then(h => {
-  handler = h;
-});
-export { handler };
+bootstrap();
